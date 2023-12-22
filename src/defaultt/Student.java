914d62f12ba75cc67efb473be6e8382a5d237115
unknown <simonastroimanova@hotmail.com>
2023-12-22 15:19:18 +0100

    Add first java program

---
+++ src/defaultt/Student.java
@@ -0,0 +1,13 @@
+// A Java which has only getter methods
+package defaultt;
+
+public class Student {
+	
+//private data member
+	private String college = "VA Skopje";
+	//getter method for college
+	public String getCollege() {
+		return college;
+	}
+			
+}
---
+++ src/defaultt/Studentt.java
@@ -0,0 +1,10 @@
+// A Java class which has only setter methods
+package defaultt;
+
+public class Studentt {
+private String college;
+//getter method for college
+public void setCollege(String college) {
+	this.college=college;
+}
+}
